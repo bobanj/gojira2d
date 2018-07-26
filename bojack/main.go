f973341f78191334808619170c5684c6a040b35c
Gesh Markov <gesh@markov.eu>
2018-07-26 11:12:03 +0200

    Added an empty window.

--- bojack/main.go
+++ bojack/main.go
@@ -1,1 +1,16 @@
-package bojack
+package main
+
+import (
+		"github.com/markov/gojira2d/pkg/app"
+		)
+
+func main() {
+	app.Init(640, 480, false, "Run For Your Life!")
+	defer app.Terminate()
+
+	app.MainLoop(func(speed float64) {
+		//NOOP
+	}, func() {
+		//NOOP
+	})
+}
