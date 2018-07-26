c07db5051ce9569a14a5f6e9b266c4ada3e4f56c
Boban Jovanoski <boban@yelp.com>
2018-07-26 15:56:52 +0200

    3 players kinda slow

--- bojack/player.go
+++ bojack/player.go
@@ -29,23 +29,24 @@ func NewPlayer(position mgl32.Vec3, scale mgl32.Vec2, playerName string, numberO
 			g.NewTextureFromFile(fmt.Sprintf("bojack/sprites/%s/%s_%s.png", playerName, playerName, spriteNumber)))
 
 	}
+	p.position = position
 	p.currentSpritePosition = 0
-	p.quad = g.NewQuadPrimitive(mgl32.Vec3{position.X(), position.Y(), 0}, mgl32.Vec2{0, 0})
+	p.quad = g.NewQuadPrimitive(position, mgl32.Vec2{0, 0})
 	p.quad.SetTexture(p.runningSprites[p.currentSpritePosition])
 	p.quad.SetSizeFromTexture()
 	p.quad.SetScale(scale)
-	p.quad.SetAnchorToCenter()
+	p.quad.SetAnchorToBottomCenter()
 	return p
 }
 
-func (p *Player) Update(speed float32) {
-	if p.currentSpritePosition == p.numberOfFrames {
+func (p *Player) Update(playerSpeed float32) {
+	if p.currentSpritePosition > p.numberOfFrames {
 		p.currentSpritePosition = 0
 	} else {
 		p.currentSpritePosition = p.currentSpritePosition + 1
 	}
 	absPos := p.position
-	absPos = absPos.Add(mgl32.Vec3{speed, 0, 0})
+	absPos = absPos.Add(mgl32.Vec3{playerSpeed, 0, 0})
 	p.position = absPos
 	p.quad.SetPosition(p.position)
 	p.quad.SetTexture(p.runningSprites[p.currentSpritePosition])
