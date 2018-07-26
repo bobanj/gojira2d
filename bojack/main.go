b79215ae9b7279c261d3404d73091856826b2be3
Gesh Markov <gesh@markov.eu>
2018-07-26 16:37:55 +0200

    First key-based speed.

--- bojack/main.go
+++ bojack/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/markov/gojira2d/pkg/app"
-	"log"
 	"github.com/go-gl/mathgl/mgl32"
 )
 
@@ -17,23 +16,46 @@ func main() {
 	defer UnregisterKeyCallback()
 	createHud()
 
-	players := make([]*Player, 0, 32)
-	players = append(players,  NewPlayer(mgl32.Vec3{40, 210, 0.001}, mgl32.Vec2{0.15, 0.15}, "bojack", 3))
-	players = append(players,  NewPlayer(mgl32.Vec3{40, 400, 0.002}, mgl32.Vec2{0.17, 0.17}, "todd", 3))
-	players = append(players,  NewPlayer(mgl32.Vec3{40, 580, 0.001}, mgl32.Vec2{0.25, 0.25}, "monkey", 3))
+	players := []*Player{
+		NewPlayer(mgl32.Vec3{40, 210, 0.001}, mgl32.Vec2{0.15, 0.15}, "bojack", glfw.KeyQ, 3),
+		NewPlayer(mgl32.Vec3{40, 400, 0.002}, mgl32.Vec2{0.17, 0.17}, "todd", glfw.KeyB, 3),
+		NewPlayer(mgl32.Vec3{40, 580, 0.001}, mgl32.Vec2{0.25, 0.25}, "monkey", glfw.KeyP, 3),
+	}
 	RegisterKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-		if action == glfw.Release {
-			log.Printf("#%d key:", key)
+		switch action {
+		case glfw.Press:
+			switch key {
+			case players[0].key:
+				players[0].keyPressed = true
+			case players[1].key:
+				players[1].keyPressed = true
+			case players[2].key:
+				players[2].keyPressed = true
+			}
+		case glfw.Release:
+
+			switch key {
+			case players[0].key:
+				players[0].keyPressed = false
+			case players[1].key:
+				players[1].keyPressed = false
+			case players[2].key:
+				players[2].keyPressed = false
+			}
 		}
 	})
 	scene := NewScene()
 
-	playerSpeed := float32(0.4)
 	app.MainLoop(func(speed float64) {
 		scene.Update(speed)
 		updateHud()
 		for _, player := range players {
-			player.Update(playerSpeed)
+			if player.keyPressed && shouldPress() {
+				player.speed += 0.1
+			} else {
+				player.speed /= 2
+			}
+			player.Update()
 		}
 	}, func() {
 		scene.Draw(app.Context)
--- bojack/player.go
+++ bojack/player.go
@@ -4,17 +4,21 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 	g "github.com/markov/gojira2d/pkg/graphics"
 	"fmt"
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
 type Player struct {
 	quad                  *g.Primitive2D
+	speed                 float32
+	key                   glfw.Key
+	keyPressed            bool
 	position              mgl32.Vec3
 	runningSprites        []*g.Texture
 	numberOfFrames        int
 	currentSpritePosition int
 }
 
-func NewPlayer(position mgl32.Vec3, scale mgl32.Vec2, playerName string, numberOfFrames int) *Player {
+func NewPlayer(position mgl32.Vec3, scale mgl32.Vec2, playerName string, key glfw.Key, numberOfFrames int) *Player {
 	p := &Player{}
 	p.runningSprites = make([]*g.Texture, 0, numberOfFrames)
 	for i := 0; i <= numberOfFrames; i++ {
@@ -29,6 +33,7 @@ func NewPlayer(position mgl32.Vec3, scale mgl32.Vec2, playerName string, numberO
 			g.NewTextureFromFile(fmt.Sprintf("bojack/sprites/%s/%s_%s.png", playerName, playerName, spriteNumber)))
 
 	}
+	p.key = key
 	p.position = position
 	p.currentSpritePosition = 0
 	p.quad = g.NewQuadPrimitive(position, mgl32.Vec2{0, 0})
@@ -39,14 +44,14 @@ func NewPlayer(position mgl32.Vec3, scale mgl32.Vec2, playerName string, numberO
 	return p
 }
 
-func (p *Player) Update(playerSpeed float32) {
+func (p *Player) Update() {
 	if p.currentSpritePosition > p.numberOfFrames {
 		p.currentSpritePosition = 0
 	} else {
 		p.currentSpritePosition = p.currentSpritePosition + 1
 	}
 	absPos := p.position
-	absPos = absPos.Add(mgl32.Vec3{playerSpeed, 0, 0})
+	absPos = absPos.Add(mgl32.Vec3{p.speed, 0, 0})
 	p.position = absPos
 	p.quad.SetPosition(p.position)
 	p.quad.SetTexture(p.runningSprites[p.currentSpritePosition])
